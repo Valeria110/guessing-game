e991de513b3b2d142ab04747205977b1e45db879
Valeriya Ivashkevich <ivashkevich03@gmail.com>
2023-08-09 20:09:31 +0300

    feat: finish the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessNum = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = Math.round((this.min + this.max) / 2);
+        return this.guessNum;
     }
 
     lower() {
-
+        return this.max = this.guessNum;
     }
 
     greater() {
-
+        return this.min = this.guessNum;
     }
 }
 
